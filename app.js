8f73a3db4c300379473772a1b01e83380383df7f
Beatriz Vieira <beatrizdcv@outlook.com>
2024-09-07 16:16:16 -0300

    deixando o jogo mais facil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Jogo do número secreto da Bia');
-let numeroMaximo = 100;
+let numeroMaximo = 50;
 let numeroSecreto = parseInt(Math.random () * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute; 
